ee2c76adcf5ec6bb154033f09ee0f2fdef7065d5
Alexander Bennett <bennett.alex.aaron@gmail.com>
2020-01-17 19:37:58 -0500

    Four of Four Exercises done

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,29 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
 
+		Scanner initialInputScanner = new Scanner(System.in);
+		System.out.println("Hello! Please input your Fibonacci number!");
+		String strInitialInput = initialInputScanner.nextLine();
+		int userInputInt = Integer.parseInt(strInitialInput);
+		
+			
+			int fiboA = 0, fiboB = 1;
+		  
+		        int i=1;
+		        while(fiboA <= userInputInt)
+		        {
+		            System.out.print(fiboA+" ");
+		            int fiboC = fiboA + fiboB;
+		            fiboA = fiboC;
+		            fiboB = fiboC;
+		            i++;
+		        }
+	 
+		}
+	 
 	}
-
-}
