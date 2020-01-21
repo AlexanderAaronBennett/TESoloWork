e6ec64f8df2987f11dafe6c84b14d96b85504ce8
Alexander Bennett <bennett.alex.aaron@gmail.com>
2020-01-20 19:30:23 -0500

    Work in Progress student exercise 06

--- module-1/06_Introduction_Objects_Strings/student-lecture/java/src/main/java/com/techelevator/Lecture.java
+++ module-1/06_Introduction_Objects_Strings/student-lecture/java/src/main/java/com/techelevator/Lecture.java
@@ -16,54 +16,70 @@ public class Lecture {
 		/* create an new instance of String using a literal */
 
 		
-		System.out.println();
-		System.out.println("******************************");
-		System.out.println("****** MEMBER METHODS ******");
-		System.out.println("******************************");
-		System.out.println();
-
-
-		/* Other commonly used methods:
-		 *
-		 * endsWith
-		 * startsWith
-		 * indexOf
-		 * lastIndexOf
-		 * length
-		 * substring
-		 * toLowerCase
-		 * toUpperCase
-		 * trim
-		 */
-
+		String greeting = "Hello, nice person!, it's a nice day!!";
+		System.out.println("greeting: " + greeting);
+//		
+//		System.out.println();
+//		System.out.println("******************************");
+//		System.out.println("****** MEMBER METHODS ******");
+//		System.out.println("******************************");
+//		System.out.println();
+//
+//
+		System.out.println("charAt(5)" + greeting.charAt(5));
+		System.out.println("indexOf(o):" + greeting.lastIndexOf("o"));
+		System.out.println("indexOf(nice)" + greeting.indexOf("nice"));
+		System.out.println("indexOf(hello)" + greeting.substring(0, 5));
+		System.out.println("indexOf(whole string)" + greeting.substring(greeting.length() -5,greeting.length()));
+		
+//		/* Other commonly used methods:
+//		 *
+//		 * endsWith
+//		 * startsWith
+//		 * indexOf
+//		 * lastIndexOf
+//		 * length
+//		 * substring
+//		 * toLowerCase
+//		 * toUpperCase
+//		 * trim
+//		 */
+//
 		System.out.println();
 		System.out.println("**********************");
 		System.out.println("****** EQUALITY ******");
 		System.out.println("**********************");
 		System.out.println();
 
-
-
-		/* Double equals will compare to see if the two variables, hello1 and
-		 * hello2 point to the same object in memory. Are they the same object? */
+//		char[]helloArray= new char[] {'H', 'E', 'L', 'L', 'O'};
+		String hello1 = "Hello";
+		String hello2 = "Hello";
+		System.out.println(hello1 + " " + hello2 + "");
+		
+		
+		
+		
+//
+//		/* Double equals will compare to see if the two variables, hello1 and
+//		 * hello2 point to the same object in memory. Are they the same object? */
 		if (hello1 == hello2) {
 			System.out.println("They are equal!");
 		} else {
 			System.out.println(hello1 + " is not equal to " + hello2);
 		}
-
-		String hello3 = hello1;
-		if (hello1 == hello3) {
-			System.out.println("hello1 is the same reference as hello3");
-		}
-
-		/* So, to compare the values of two objects, we need to use the equals method.
-		 * Every object type has an equals method */
-		if (hello1.equals(hello2)) {
-			System.out.println("They are equal!");
-		} else {
-			System.out.println(hello1 + " is not equal to " + hello2);
-		}
-
+//
+//		String hello3 = hello1;
+//		if (hello1 == hello3) {
+//			System.out.println("hello1 is the same reference as hello3");
+//		}
+//
+//		/* So, to compare the values of two objects, we need to use the equals method.
+//		 * Every object type has an equals method */
+//		if (hello1.equals(hello2)) {
+//			System.out.println("They are equal!");
+//		} else {
+//			System.out.println(hello1 + " is not equal to " + hello2);
+//		}
+//
 	}
 }
