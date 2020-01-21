e6ad3092e8b0da3914086e45f74af423e288e9d4
Alexander Bennett <bennett.alex.aaron@gmail.com>
2020-01-21 18:54:11 -0500

    Two thirds of 06 complete

--- module-1/06_Introduction_Objects_Strings/student-exercise/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/06_Introduction_Objects_Strings/student-exercise/java/src/main/java/com/techelevator/Exercises.java
@@ -2,27 +2,27 @@ package com.techelevator;
 
 public class Exercises {
 
-	/*1
-	 * Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
-	 * helloName("Bob") → "Hello Bob!" helloName("Alice") → "Hello Alice!"
-	 * helloName("X") → "Hello X!"
+	/*
+	 * 1 Given a string name, e.g. "Bob", return a greeting of the form
+	 * "Hello Bob!". helloName("Bob") → "Hello Bob!" helloName("Alice") →
+	 * "Hello Alice!" helloName("X") → "Hello X!"
 	 */
 	public String helloName(String name) {
 		return ("Hello " + name + "!");
 	}
 
-	/*2
-	 * Given two strings, a and b, return the result of putting them together in the
-	 * order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi". makeAbba("Hi", "Bye") →
-	 * "HiByeByeHi" makeAbba("Yo", "Alice") → "YoAliceAliceYo" makeAbba("What",
-	 * "Up") → "WhatUpUpWhat"
+	/*
+	 * 2 Given two strings, a and b, return the result of putting them together in
+	 * the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi". makeAbba("Hi",
+	 * "Bye") → "HiByeByeHi" makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+	 * makeAbba("What", "Up") → "WhatUpUpWhat"
 	 */
 	public String makeAbba(String a, String b) {
 		return (a + b + b + a);
 	}
 
-	/*3
-	 * The web is built with HTML strings like "<i>Yay</i>" which draws Yay as
+	/*
+	 * 3 The web is built with HTML strings like "<i>Yay</i>" which draws Yay as
 	 * italic text. In this example, the "i" tag makes <i> and </i> which surround
 	 * the word "Yay". Given tag and word strings, create the HTML string with tags
 	 * around the word, e.g. "<i>Yay</i>". makeTags("i", "Yay") → "<i>Yay</i>"
@@ -33,8 +33,8 @@ public class Exercises {
 		return ("<" + tag + ">" + word + "</" + tag + ">");
 	}
 
-	/*4
-	 * Given an "out" string length 4, such as "<<>>", and a word, return a new
+	/*
+	 * 4 Given an "out" string length 4, such as "<<>>", and a word, return a new
 	 * string where the word is in the middle of the out string, e.g. "<<word>>".
 	 * Note: use str.substring(i, j) to extract the String starting at index i and
 	 * going up to but not including index j. makeOutWord("<<>>", "Yay") → "<<Yay>>"
@@ -45,22 +45,21 @@ public class Exercises {
 		return out.substring(0, 2) + word + out.substring(2);
 	}
 
-	/*5
-	 * Given a string, return a new string made of 3 copies of the last 2 chars of
+	/*
+	 * 5 Given a string, return a new string made of 3 copies of the last 2 chars of
 	 * the original string. The string length will be at least 2. extraEnd("Hello")
 	 * → "lololo" extraEnd("ab") → "ababab" extraEnd("Hi") → "HiHiHi"
 	 */
-	public String extraEnd(String str) 
-	{
-		return str.substring(str.length()-2) + str.substring(str.length() -2) + str.substring(str.length()-2);
+	public String extraEnd(String str) {
+		return str.substring(str.length() - 2) + str.substring(str.length() - 2) + str.substring(str.length() - 2);
 	}
 
-	/*6
-	 * Given a string, return the string made of its first two chars, so the String
-	 * "Hello" yields "He". If the string is shorter than length 2, return whatever
-	 * there is, so "X" yields "X", and the empty string "" yields the empty string
-	 * "". Note that str.length() returns the length of a string. firstTwo("Hello")
-	 * → "He" firstTwo("abcdefg") → "ab" firstTwo("ab") → "ab"
+	/*
+	 * 6 Given a string, return the string made of its first two chars, so the
+	 * String "Hello" yields "He". If the string is shorter than length 2, return
+	 * whatever there is, so "X" yields "X", and the empty string "" yields the
+	 * empty string "". Note that str.length() returns the length of a string.
+	 * firstTwo("Hello") → "He" firstTwo("abcdefg") → "ab" firstTwo("ab") → "ab"
 	 */
 	public String firstTwo(String str) {
 		if (str.length() >= 2) {
@@ -72,28 +71,29 @@ public class Exercises {
 		}
 	}
 
-	/*7
-	 * Given a string of even length, return the first half. So the string "WooHoo"
-	 * yields "Woo". firstHalf("WooHoo") → "Woo" firstHalf("HelloThere") → "Hello"
-	 * firstHalf("abcdef") → "abc"
+	/*
+	 * 7 Given a string of even length, return the first half. So the string
+	 * "WooHoo" yields "Woo". firstHalf("WooHoo") → "Woo" firstHalf("HelloThere") →
+	 * "Hello" firstHalf("abcdef") → "abc"
 	 */
 	public String firstHalf(String str) {
 		return str.substring(0, str.length() / 2);
 	}
 
-	/*8
-	 * Given a string, return a version without the first and last char, so "Hello"
-	 * yields "ell". The string length will be at least 2. withoutEnd("Hello") →
-	 * "ell" withoutEnd("java") → "av" withoutEnd("coding") → "odin"
+	/*
+	 * 8 Given a string, return a version without the first and last char, so
+	 * "Hello" yields "ell". The string length will be at least 2.
+	 * withoutEnd("Hello") → "ell" withoutEnd("java") → "av" withoutEnd("coding") →
+	 * "odin"
 	 */
 	public String withoutEnd(String str) {
 		return str.substring(1, str.length() - 1);
 	}
 
-	/*9
-	 * Given 2 strings, a and b, return a string of the form short+long+short, with
-	 * the shorter string on the outside and the longer string on the inside. The
-	 * strings will not be the same length, but they may be empty (length 0).
+	/*
+	 * 9 Given 2 strings, a and b, return a string of the form short+long+short,
+	 * with the shorter string on the outside and the longer string on the inside.
+	 * The strings will not be the same length, but they may be empty (length 0).
 	 * comboString("Hello", "hi") → "hiHellohi" comboString("hi", "Hello") →
 	 * "hiHellohi" comboString("aaa", "b") → "baaab"
 	 */
@@ -107,8 +107,8 @@ public class Exercises {
 
 	}
 
-	/*10
-	 * Given 2 strings, return their concatenation, except omit the first char of
+	/*
+	 * 10 Given 2 strings, return their concatenation, except omit the first char of
 	 * each. The strings will be at least length 1. nonStart("Hello", "There") →
 	 * "ellohere" nonStart("java", "code") → "avaode" nonStart("shotl", "java") →
 	 * "hotlava"
@@ -117,26 +117,26 @@ public class Exercises {
 		return (a.substring(1) + b.substring(1));
 	}
 
-	/*11
-	 * Given a string, return a "rotated left 2" version where the first 2 chars are
-	 * moved to the end. The string length will be at least 2. left2("Hello") →
+	/*
+	 * 11 Given a string, return a "rotated left 2" version where the first 2 chars
+	 * are moved to the end. The string length will be at least 2. left2("Hello") →
 	 * "lloHe" left2("java") → "vaja" left2("Hi") → "Hi"
 	 */
 	public String left2(String str) {
 		return (str.substring(2) + str.subSequence(0, 2));
 	}
 
-	/*12
-	 * Given a string, return a "rotated right 2" version where the last 2 chars are
-	 * moved to the start. The string length will be at least 2. right2("Hello") →
-	 * "loHel" right2("java") → "vaja" right2("Hi") → "Hi"
+	/*
+	 * 12 Given a string, return a "rotated right 2" version where the last 2 chars
+	 * are moved to the start. The string length will be at least 2. right2("Hello")
+	 * → "loHel" right2("java") → "vaja" right2("Hi") → "Hi"
 	 */
 	public String right2(String str) {
-		return (str.substring(str.length()-2) + str.substring(0, str.length()-2));
+		return (str.substring(str.length() - 2) + str.substring(0, str.length() - 2));
 	}
 
-	/*13
-	 * Given a string, return a string length 1 from its front, unless front is
+	/*
+	 * 13 Given a string, return a string length 1 from its front, unless front is
 	 * false, in which case return a string length 1 from its back. The string will
 	 * be non-empty. theEnd("Hello", true) → "H" theEnd("Hello", false) → "o"
 	 * theEnd("oh", true) → "o"
@@ -148,32 +148,32 @@ public class Exercises {
 		return (str.substring(str.length() - 1));
 	}
 
-	/* 14
-	 * Given a string, return a version without both the first and last char of the
-	 * string. The string may be any length, including 0. withoutEnd2("Hello") →
+	/*
+	 * 14 Given a string, return a version without both the first and last char of
+	 * the string. The string may be any length, including 0. withoutEnd2("Hello") →
 	 * "ell" withoutEnd2("abc") → "b" withoutEnd2("ab") → ""
 	 */
 	public String withoutEnd2(String str) {
-	if (str.length() > 2){
-		return str.substring(1, str.length()-1);
-	}
-	{
-	return "";
-	}
+		if (str.length() > 2) {
+			return str.substring(1, str.length() - 1);
+		}
+		{
+			return "";
+		}
 	}
 
-	/*15
-	 * Given a string of even length, return a string made of the middle two chars,
-	 * so the string "string" yields "ri". The string length will be at least 2.
-	 * middleTwo("string") → "ri" middleTwo("code") → "od" middleTwo("Practice") →
-	 * "ct"
+	/*
+	 * 15 Given a string of even length, return a string made of the middle two
+	 * chars, so the string "string" yields "ri". The string length will be at least
+	 * 2. middleTwo("string") → "ri" middleTwo("code") → "od" middleTwo("Practice")
+	 * → "ct"
 	 */
 	public String middleTwo(String str) {
 		return (str.substring(str.length() / 2 - 1, str.length() / 2 + 1));
 	}
 
-	/*16
-	 * Given a string, return true if it ends in "ly". endsLy("oddly") → true
+	/*
+	 * 16 Given a string, return true if it ends in "ly". endsLy("oddly") → true
 	 * endsLy("y") → false endsLy("oddy") → false
 	 */
 	public boolean endsLy(String str) {
@@ -185,8 +185,8 @@ public class Exercises {
 		}
 	}
 
-	/*17
-	 * Given a string and an int n, return a string made of the first and last n
+	/*
+	 * 17 Given a string and an int n, return a string made of the first and last n
 	 * chars from the string. The string length will be at least n. nTwice("Hello",
 	 * 2) → "Helo" nTwice("Chocolate", 3) → "Choate" nTwice("Chocolate", 1) → "Ce"
 	 */
@@ -194,11 +194,11 @@ public class Exercises {
 		return str.substring(0, n) + str.substring(str.length() - n, str.length());
 	}
 
-	/*18
-	 * Given a string and an index, return a string length 2 starting at the given
-	 * index. If the index is too big or too small to define a string length 2, use
-	 * the first 2 chars. The string length will be at least 2. twoChar("java", 0) →
-	 * "ja" twoChar("java", 2) → "va" twoChar("java", 3) → "ja"
+	/*
+	 * 18 Given a string and an index, return a string length 2 starting at the
+	 * given index. If the index is too big or too small to define a string length
+	 * 2, use the first 2 chars. The string length will be at least 2.
+	 * twoChar("java", 0) → "ja" twoChar("java", 2) → "va" twoChar("java", 3) → "ja"
 	 */
 	public String twoChar(String str, int index) {
 		if (index + 2 > str.length()) {
@@ -213,53 +213,69 @@ public class Exercises {
 	}
 
 	/*
-	 * Given a string of odd length, return the string length 3 from its middle, so
-	 * "Candy" yields "and". The string length will be at least 3.
+	 * 19 Given a string of odd length, return the string length 3 from its middle,
+	 * so "Candy" yields "and". The string length will be at least 3.
 	 * middleThree("Candy") → "and" middleThree("and") → "and"
 	 * middleThree("solving") → "lvi"
 	 */
 	public String middleThree(String str) {
-		if (str.length() <= 3) {
-			return str;
-		}
-		{
-			return str.substring(str.length() % 3, str.length () -1);
-		}
+		return str.substring(str.length() / 2 - 1, str.length() / 2 + 2);
+
 	}
 
 	/*
-	 * Given a string, return true if "bad" appears starting at index 0 or 1 in the
-	 * string, such as with "badxxx" or "xbadxx" but not "xxbadxx". The string may
-	 * be any length, including 0. Note: use .equals() to compare 2 strings.
+	 * 20 Given a string, return true if "bad" appears starting at index 0 or 1 in
+	 * the string, such as with "badxxx" or "xbadxx" but not "xxbadxx". The string
+	 * may be any length, including 0. Note: use .equals() to compare 2 strings.
 	 * hasBad("badxx") → true hasBad("xbadxx") → true hasBad("xxbadxx") → false
 	 */
 	public boolean hasBad(String str) {
-		if (str.substring(0,3).equals("bad") || str.substring(1, 4).equals("bad")) {
+		if (str.indexOf("bad") == 0) {
 			return true;
 		}
-		{
-			return false;
+
+		if (str.indexOf("bad") == 1) {
+			return true;
 		}
+
+		return false;
+
 	}
-	
 
 	/*
-	 * Given a string and a non-negative int n, return a larger string that is n
+	 * 21 Given a string and a non-negative int n, return a larger string that is n
 	 * copies of the original string. stringTimes("Hi", 2) → "HiHi"
 	 * stringTimes("Hi", 3) → "HiHiHi" stringTimes("Hi", 1) → "Hi"
 	 */
 	public String stringTimes(String str, int n) {
-		return null;
+		String stringSum = "";
+
+		for (int i = 0; i < n; i++) {
+			stringSum = stringSum + str;
+		}
+
+		return stringSum;
 	}
 
-	/*
+	/*22
 	 * Given a string and a non-negative int n, we'll say that the front of the
 	 * string is the first 3 chars, or whatever is there if the string is less than
 	 * length 3. Return n copies of the front; frontTimes("Chocolate", 2) → "ChoCho"
 	 * frontTimes("Chocolate", 3) → "ChoChoCho" frontTimes("Abc", 3) → "AbcAbcAbc"
 	 */
 	public String frontTimes(String str, int n) {
-		return null;
+		String stringSum = "";
+		if (str.length() > 3) {
+			for (int i = 0; i < n; i++) {
+				stringSum = stringSum + str.substring(0, 3);
+			}
+
+			return stringSum;
+		}
+		for (int i = 0; i < n; i++) {
+			stringSum = stringSum + str.substring(0);
+		}
+		return stringSum;
 	}
 
 	/*
@@ -268,7 +284,13 @@ public class Exercises {
 	 * countXX("xxxx") →
 	 */
 	public int countXX(String str) {
-		return 0;
+		int counter = 0;
+	if (str.contains("xx")){	
+		for (int i = 0; i < str.length() ; i ++) {
+			counter = counter + 1;
+		}
+		return [i];
+	}
 	}
 
 	/*
