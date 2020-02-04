5d2114d5afcfc81f06822563332df7adafbcfb9a
Alexander Bennett <bennett.alex.aaron@gmail.com>
2020-02-04 18:02:11 -0500

    WordSearch Part 1 Complete

--- module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -1,5 +1,20 @@
 package com.techelevator;
 
 public class KataFizzBuzz {
+	public static String Fizzbuzz(int result) {
+		if (result < 0 || result > 100) {
+			return "";
+		}
+
+		if (result % 3 == 0 && result % 5 == 0||Integer.toString(result).contains("3") && Integer.toString(result).contains("5")) {
+			return "FizzBuzz";
+		} else if (result % 5 == 0||Integer.toString(result).contains("5")) {
+			return "Buzz";
+		} else if (result % 3 == 0||Integer.toString(result).contains("3")) {
+			return "Fizz";
+		}else {
+			return Integer.toString(result);
+		}
+	}
 
 }
