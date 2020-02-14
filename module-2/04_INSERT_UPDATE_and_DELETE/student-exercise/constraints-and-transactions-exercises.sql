2a0138a48ad5a739c00eef648ca5d647660e9c96
Alexander Bennett <bennett.alex.aaron@gmail.com>
2020-02-14 07:52:17 -0500

    module2exercise04 11/12 complete

--- module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
@@ -3,44 +3,85 @@
 
 -- 1. Add actors, Hampton Avenue, and Lisa Byway to the actor table.
 
+BEGIN TRANSACTION;
+INSERT INTO actor(first_name,last_name)
+VALUES ('Hampton', 'Avenue'), ('Lisa','Byway')
+COMMIT TRANSACTION;
+
 -- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in
 -- ancient Greece", to the film table. The movie was released in 2008 in English.
 -- Since its an epic, the run length is 3hrs and 18mins. There are no special
 -- features, the film speaks for itself, and doesn't need any gimmicks.
-
+BEGIN TRANSACTION;
+INSERT INTO film(title, description, release_year, language_id,length)
+VALUES ('Euclidean PI', 'The epic story of Euclid as a pizza delivery boy in ancient Greece', 2008, 1, 198)
+COMMIT TRANSACTION;
 -- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly
 -- overprotective mother, in the film, "Euclidean PI". Add them to the film.
-
+BEGIN TRANSACTION;
+INSERT INTO film_actor(actor_id, film_id)
+VALUES ((SELECT actor_id FROM actor WHERE first_name = 'Hampton' AND last_name ='Avenue'),(SELECT film_id FROM film WHERE title = 'Euclidean PI')),
+        ((SELECT actor_id FROM actor WHERE first_name = 'Lisa' AND last_name = 'Byway'), (SELECT film_id FROM film WHERE title = 'Euclidean PI'))
+                                               
+COMMIT TRANSACTION;
 -- 4. Add Mathmagical to the category table.
+BEGIN TRANSACTION;
+INSERT INTO category(name)
+VALUES('Mathmagical')
+
+COMMIT TRANSACTION;
 
 -- 5. Assign the Mathmagical category to the following films, "Euclidean PI",
 -- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
-
+BEGIN TRANSACTION;
+INSERT INTO film_category(film_id, category_id)
+VALUES  ((SELECT film_id FROM film WHERE title = 'Euclidean PI'), (SELECT category_id FROM category WHERE name = 'Mathmagical')),
+        ((SELECT film_id FROM film WHERE title = 'EGG IGBY'), (SELECT category_id FROM category WHERE name = 'Mathmagical')),
+        ((SELECT film_id FROM film WHERE title = 'KARATE MOON'), (SELECT category_id FROM category WHERE name = 'Mathmagical')),
+        ((SELECT film_id FROM film WHERE title = 'RANDOM GO'), (SELECT category_id FROM category WHERE name = 'Mathmagical')),
+        ((SELECT film_id FROM film WHERE title = 'YOUNG LANGUAGE'), (SELECT category_id FROM category WHERE name = 'Mathmagical'))
+COMMIT TRANSACTION;
 -- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films
 -- accordingly.
 -- (5 rows affected)
 
--- 7. Add a copy of "Euclidean PI" to all the stores.
 
+
+
+-- 7. Add a copy of "Euclidean PI" to all the stores.
+BEGIN TRANSACTION;
+INSERT INTO inventory(film_id, store_id)
+VALUES ((SELECT film_id FROM film WHERE title = 'Euclidean PI'), (SELECT store_id FROM store WHERE store_id = 1)),
+        ((SELECT film_id FROM film WHERE title = 'Euclidean PI'), (SELECT store_id FROM store WHERE store_id = 2))
+COMMIT TRANSACTION;
 -- 8. The Feds have stepped in and have impounded all copies of the pirated film,
 -- "Euclidean PI". The film has been seized from all stores, and needs to be
 -- deleted from the film table. Delete "Euclidean PI" from the film table.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
-
+-- <No, because there is a foreign key constraint >
+BEGIN;
+DELETE FROM film WHERE title = 'Euclidian PI';
+COMMIT;
 -- 9. Delete Mathmagical from the category table.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
-
+-- <No, because there is a foreign key constraint>
+BEGIN;
+DELETE FROM category WHERE name = 'Mathmagical';
+COMMIT;
 -- 10. Delete all links to Mathmagical in the film_category tale.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
-
+-- <NO, because category_id is connected as a foreign key to another table>
+BEGIN;
+DELETE FROM film_category WHERE category_id = 17;
+COMMIT;
 -- 11. Retry deleting Mathmagical from the category table, followed by retrying
 -- to delete "Euclidean PI".
 -- (Did either deletes succeed? Why?)
--- <YOUR ANSWER HERE>
-
+-- <NO because there is a foreign keys connection to actors>
+BEGIN;
+DELETE FROM category WHERE name = 'Mathmagical';
+DELETE FROM film WHERE title = 'Euclidian PI';
+ROLLBACK;
 -- 12. Check database metadata to determine all constraints of the film id, and
 -- describe any remaining adjustments needed before the film "Euclidean PI" can
 -- be removed from the film table.
--- module-2/04_INSERT_UPDATE_and_DELETE/student-lecture/lecture-postgresql.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/student-lecture/lecture-postgresql.sql
@@ -1,32 +1,51 @@
 -- INSERT
 
 -- 1. Add Klingon as a spoken language in the USA
+INSERT INTO countrylanguage(countrycode, language, isofficial,percentage)
+VALUES('USA', 'Klingon', false, 3.0);
 -- 2. Add Klingon as a spoken language in Great Britain
-
-
+INSERT INTO countrylanguage(countrycode, language, isofficial,percentage)
+VALUES('GBR', 'Klingon', true, 50.0);
 -- UPDATE
-
+SELECT city.name
+FROM city
+JOIN country ON country.capital = city.id
+WHERE country.code = 'USA';
 -- 1. Update the capital of the USA to Houston
+UPDATE country
+SET capital =   (SELECT id 
+                FROM city
+                WHERE name = 'Houston')
+WHERE code = 'USA';
 -- 2. Update the capital of the USA to Washington DC and the head of state
-
+UPDATE country
+SET capital =   (SELECT id 
+                FROM city
+                WHERE name = 'Washington'),
+      headofstate = 'Derek Leach'
+      WHERE code = 'USA'
 
 -- DELETE
 
 -- 1. Delete English as a spoken language in the USA
+SELECT * FROM countrylanguage WHERE countrycode = 'USA' AND language = 'English';
+DELETE FROM countrylanguage WHERE countrycode = 'USA' AND language = 'English';
 -- 2. Delete all occurrences of the Klingon language 
-
+SELECT * FROM countrylanguage WHERE language = 'Klingon';
+DELETE FROM countrylanguage WHERE language = 'Klingon';
 
 -- REFERENTIAL INTEGRITY
 
 -- 1. Try just adding Elvish to the country language table.
-
+INSERT INTO countrylanguage (language)
 -- 2. Try inserting English as a spoken language in the country ZZZ. What happens?
-
+INSERT INTO countrylanguage(countrycode, language, isofficial, percentage)
+VALUES ('ZZZ', 'English', true, 1.0);
 -- 3. Try deleting the country USA. What happens?
-
+DELETE FROM country WHERE code = 'USA';
 
 -- CONSTRAINTS
-
+SELECT *FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
 -- 1. Try inserting English as a spoken language in the USA
 
 -- 2. Try again. What happens?
@@ -44,6 +63,11 @@ SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS
 -- TRANSACTIONS
 
 -- 1. Try deleting all of the rows from the country language table and roll it back.
+BEGIN TRANSACTION;
+
+DELETE FROM countrylanguage;
+
+ROLLBACK TRANSACTION;
 
 -- 2. Try updating all of the cities to be in the USA and roll it back
 
