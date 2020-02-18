b0747059ce945a5a065c27b693eb311ad6342c15
Alexander Bennett <bennett.alex.aaron@gmail.com>
2020-02-18 08:12:12 -0500

    module2_exercise5complete

--- module-2/05_Database_Design/student-exercise/projects.sql
+++ module-2/05_Database_Design/student-exercise/projects.sql
@@ -48,13 +48,28 @@ COMMIT;
 
 BEGIN;
 INSERT INTO employee (employee_number, job_title, first_name, last_name, gender, date_of_birth, date_of_hire, employee_department)
-VALUES ('Director Of Human Resources', 'Hugh', 'Mann', NULL , '1947-06-14', '2000-01-01' , 1),
-       ('Human Resources Officer', 'Betty', 'Gray', 'female' , '1989-10-01', '2010-06-05' , 1),
-       ('Head Accountant', 'Andrew', 'McMurray', 'male' , '1989-06-27', '2017-09-17' , 2),
-       ('Accountant', 'Lenny', 'Poorman', 'male' , '1994-08-22', '2019-09-29' , 2),
-       ('Scrum Master', 'Trevor', 'Tenessee', 'male' , '1991-08-22', '2020-01-13' , 3),
-       ('Head Technician', 'Chris', 'Chicken', NULL , '2019-09-09', '2020-02-13' , 3),
-        ('The White', 'Gandalf', 'Thegray', male , '0002-01-01', '0003-02-14' , 4),
-        ('Archmage', 'The', 'Dragonborn', male , '0777-07-07', '0012-12-12' , 4)
-       
+VALUES (1,'Director Of Human Resources', 'Hugh', 'Mann', NULL , '1947-06-14', '2000-01-01' , 1),
+       (2,'Human Resources Officer', 'Betty', 'Gray', 'female' , '1989-10-01', '2010-06-05' , 1),
+       (3, 'Head Accountant', 'Andrew', 'McMurray', 'male' , '1989-06-27', '2017-09-17' , 2),
+       (4, 'Accountant', 'Lenny', 'Poorman', 'male' , '1994-08-22', '2019-09-29' , 2),
+       (5, 'Scrum Master', 'Trevor', 'Tenessee', 'male' , '1991-08-22', '2020-01-13' , 3),
+       (6, 'Head Technician', 'Chris', 'Chicken', NULL , '2019-09-09', '2020-02-13' , 3),
+        (7, 'The White', 'Gandalf', 'Thegray', 'male' , '0002-01-01', '0003-02-14' , 4),
+        (8, 'Archmage', 'The', 'Dragonborn', 'male' , '0777-07-07', '0012-12-12' , 4)
 COMMIT;
+
+BEGIN;
+INSERT INTO project (project_number, project_name, project_start_date)
+VALUES (1,'Icarus', '1990-10-15'), (2,'Lazarus','1999-10-19'), (3,'annual budget', '2019-12-01'), (4,'DEFEAT THE DARK LORD', '2008-05-09');
+COMMIT;
+
+BEGIN;
+INSERT INTO employee_project (employee_number,project_number)
+VALUES (1,1),
+       (2,2),
+       (3,3),
+       (4,3),
+       (5,1),
+       (6,2),
+       (7,4),
+       (8,4)
