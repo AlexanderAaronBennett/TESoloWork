f92fbbcd0a56d6c68f69295ef99194fece2d0bb6
Alexander Bennett <bennett.alex.aaron@gmail.com>
2020-03-27 08:43:48 -0400

    Intro to Vue Exercise complete

---
+++ module-4/54_Introduction_to_Vue/student-exercise/shopping-list/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="shoppingList-app">
+    <shopping-list></shopping-list>
+  </div>
+</template>
+
+<script>
+import ShoppingList from './components/ShoppingList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ShoppingList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
